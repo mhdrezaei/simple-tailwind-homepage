dc8d127195dbf83d4106b5e73825314633cd7e7f
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2023-02-19 16:18:24 +0330

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        srtrongCyan: "hsl(171 , 66% , 44%)",
+        lightBlue: "hsl(233 , 100% , 69%)",
+        darkGrayishBlue: "hsl(210 , 10% , 33%)",
+        grayishBlue: "hsl(201 , 11% , 66%)",
+      },
+      fontFamily: {
+        sans: ["Bai jamjuree", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
